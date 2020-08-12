719eff46bc9bfe6284a56626d6eac2f2025ab265
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-12 03:20:59 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,4 @@
 class User 
-  def first_name
-    @fist_name = first_name
-  end 
-  def last_name
-    @last_name = last_name
-  end
+attr_reader :first_name, :last_name
+attr_writer :first_name, :last_name
 end
