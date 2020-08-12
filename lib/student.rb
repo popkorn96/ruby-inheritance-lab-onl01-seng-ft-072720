9cca0b34ac27ca06f99844b21fa602a0fd404998
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-12 03:44:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,7 @@
 class Student < User 
-attr_accessor :knowledge
-def initialize(knowledge)
-  @knowledge = knowledge
-  knowledge = []
-end
+
+  @@knowledge = []
+
 def learn(string)
   knowledge << string
 end
